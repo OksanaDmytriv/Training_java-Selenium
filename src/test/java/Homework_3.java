8b1d1b9db233f2d721fe8c98231fc99937c06f82
dmytriv.oksana@gmail.com <b777@370Lv>
2018-05-21 21:10:26 +0300

    add more asserts

--- src/test/java/Homework_3.java
+++ src/test/java/Homework_3.java
@@ -4,20 +4,20 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-
 public class Homework_3 {
     static WebDriver driver;
 
-    public WebElement findElementByXpathOnMainPage(String locator) {
+    public WebElement findByXpathMainPage(String locator) {
         return driver.findElement(By.xpath("//div[@id='box-campaigns']//ul[@class='listing-wrapper products']/li[1]" + locator));
     }
 
+    public WebElement findByClassName(String locator) {
+        return driver.findElement(By.className(locator));
+    }
 
     @BeforeClass
     public static void start() {
@@ -31,22 +31,49 @@ public class Homework_3 {
 
         //main page
         String mainPageText = driver.findElement(By.cssSelector("#box-campaigns .name")).getText();
-        String discountPriceMainPageStr = findElementByXpathOnMainPage("//strong[@class='campaign-price']").getText();
-        String regularPriceMainPageStr = findElementByXpathOnMainPage("//s[@class='regular-price']").getText();
+        String discountPriceMainPage = findByXpathMainPage("//strong[@class='campaign-price']").getText();
+
+        String textStyleDiscountPriceMainPage = findByXpathMainPage("//strong[@class='campaign-price']")
+                .getTagName();
+        String colorDiscountPriceMainPage = findByXpathMainPage("//strong[@class='campaign-price']")
+                .getCssValue("color");
+
+        String regularPriceMainPage = findByXpathMainPage("//s[@class='regular-price']").getText();
+
+        String textStyleRegularPriceMainPage = findByXpathMainPage("//s[@class='regular-price']")
+                .getTagName();
+        String colorRegularPriceMainPage = findByXpathMainPage("//s[@class='regular-price']")
+                .getCssValue("color");
 
 
         //product page
-        findElementByXpathOnMainPage("").click();
+        findByXpathMainPage("").click();
 
         String productPageText = driver.findElement(By.cssSelector("#box-product .title")).getText();
-        String discountPriceProductPageStr = driver.findElement(By.className("campaign-price")).getText();
-        String regularPriceProductPageStr = driver.findElement(By.className("regular-price")).getText();
+        String discountPriceProductPage = findByClassName("campaign-price").getText();
+
+        String textStyleDiscountPriceProductPage = findByClassName("campaign-price").getTagName();
+        String colorDiscountPriceProductPage = findByClassName("campaign-price")
+                .getCssValue("color");
+
+        String regularPriceProductPage = driver.findElement(By.className("regular-price")).getText();
+
+        String textStyleRegularPriceProductPage = findByClassName("regular-price").getTagName();
+        String colorRegularPriceProductPage = findByClassName("regular-price")
+                .getCssValue("color");
 
 
         //asserts
         Assert.assertEquals(mainPageText, productPageText);
-        Assert.assertEquals(discountPriceMainPageStr, discountPriceProductPageStr);
-        Assert.assertEquals(regularPriceMainPageStr, regularPriceProductPageStr);
+
+        Assert.assertEquals(discountPriceMainPage, discountPriceProductPage);
+        Assert.assertEquals(regularPriceMainPage, regularPriceProductPage);
+
+        Assert.assertEquals(textStyleDiscountPriceMainPage, textStyleDiscountPriceProductPage);
+        Assert.assertEquals(colorDiscountPriceMainPage,colorDiscountPriceProductPage);
+
+        Assert.assertEquals(textStyleRegularPriceMainPage, textStyleRegularPriceProductPage);
+        Assert.assertEquals(colorRegularPriceMainPage, colorRegularPriceProductPage);
     }
 
     @AfterClass
